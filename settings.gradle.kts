0c40c34c2e43115601dae37089656bee1ec169bc
GitHub Action <action@github.com>
2021-02-15 20:47:48 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "find-immutables-usages-plugin"
---
+++ src/main/kotlin/com/github/plaskowski/findimmutablesusagesplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.plaskowski.findimmutablesusagesplugin.services
+
+import com.github.plaskowski.findimmutablesusagesplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
